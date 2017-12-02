cc5d75d64cfc423859bef1f2de40192e66a1a154
WizrdJim <wzrdjim@gmail.com>
2017-12-02 15:04:48 -0500

    This is my first personal agile project everything is built around deployment. Imported work from previous boilerplate dead end.

--- server/index.js
+++ server/index.js
@@ -1,16 +1,25 @@
 const express = require('express');
+const mongoose = require('mongoose');
+const bodyParser = require('body-parser');
+const cors = require('cors');
+const morgan = require('morgan');
+const routes = require('./routes');
 const path = require('path');
 
 const app = express();
+var cool = require('cool-ascii-faces');
+
 const PORT = process.env.PORT || 5000;
 
 // Priority serve any static files.
 app.use(express.static(path.resolve(__dirname, '../react-ui/build')));
+app.use('/API', routes);
 
 // Answer API requests.
 app.get('/api', function (req, res) {
   res.set('Content-Type', 'application/json');
-  res.send('{"message":"Hello from the custom server!"}');
+  res.send(`{"message":"Hello from the custom server!${cool()}"}`);
+
 });
 
 // All remaining requests return the React app, so it can handle routing.
